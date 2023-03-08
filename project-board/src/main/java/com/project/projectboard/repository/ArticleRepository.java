f0fb08564a4834d9ff0bb186e8d592e258debf83
sarang682 <jms20914@gmail.com>
2023-03-08 23:49:36 +0900

    서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작
    나머지 로직의 테스트 코드를 작성하고 연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- project-board/src/main/java/com/project/projectboard/repository/ArticleRepository.java
+++ project-board/src/main/java/com/project/projectboard/repository/ArticleRepository.java
@@ -4,6 +4,8 @@ import com.project.projectboard.Domain.Article;
 import com.project.projectboard.Domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -16,6 +18,7 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>, // 검색기능
         QuerydslBinderCustomizer<QArticle>
 {
+    Page<Article> findByTitle(String title, Pageable pageable);
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);
--- project-board/src/main/java/com/project/projectboard/service/ArticleService.java
+++ project-board/src/main/java/com/project/projectboard/service/ArticleService.java
@@ -2,13 +2,13 @@ package com.project.projectboard.service;
 
 import com.project.projectboard.Domain.type.SearchType;
 import com.project.projectboard.dto.ArticleDto;
-import com.project.projectboard.dto.ArticleUpdateDto;
+import com.project.projectboard.dto.ArticleWithCommentsDto;
 import com.project.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -18,21 +18,22 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long articleId) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
     }
+
 }
