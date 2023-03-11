de78e25dbbdd3b8805d8786c121e9fcf2629872e
sarang682 <jms20914@gmail.com>
2023-03-11 22:41:16 +0900

    #35 - data.sql 수정
    회원 정보 테스트 데이터 삽입 문법에 문제가 있는 것을 발견하고 수정해주었다.

--- project-board/src/main/resources/data.sql
+++ project-board/src/main/resources/data.sql
@@ -2,8 +2,8 @@
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at,
                           modified_by)
-values ('sarang', 'asdf1234', '', 'sarang682@mail.com', 'I am Sarang.', now(), 'sarang', now(), 'sarang');
-values ('sarang2', 'asdf1234', '', 'sarang6822@mail.com', 'I am Sarang2.', now(), 'sarang2', now(), 'sarang2');
+values ('sarang', 'asdf1234', '', 'sarang682@mail.com', 'I am Sarang.', now(), 'sarang', now(), 'sarang'),
+('sarang2', 'asdf1234', '', 'sarang6822@mail.com', 'I am Sarang2.', now(), 'sarang2', now(), 'sarang2');
 
 -- 123 게시글
 insert into article (user_account_id, title, content, hashtag, created_by, modified_by, created_at, modified_at)
