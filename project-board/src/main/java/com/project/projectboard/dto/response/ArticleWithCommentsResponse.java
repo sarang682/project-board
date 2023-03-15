ba2c4cd2f4d2649e144428fbe3fa686f41545ebd
sarang682 <jms20914@gmail.com>
2023-03-15 22:08:45 +0900

    DTO 들에 Serializable 인터페이스 제거
    JPA Buddy 를 이용해 작성한 DTO들인데,
    자동으로 `implements Serializable` 이 들어가버림
    우리 프로젝트는 직렬화로 Jackson을 사용하므로
    필요하지 않고, 의도하여 넣은 코드도 아님
    그러므로 삭제

--- project-board/src/main/java/com/project/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ project-board/src/main/java/com/project/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -2,7 +2,6 @@ package com.project.projectboard.dto.response;
 
 import com.project.projectboard.dto.ArticleWithCommentsDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -17,7 +16,7 @@ public record ArticleWithCommentsResponse(
         String email,
         String nickname,
         Set<ArticleCommentResponse> articleCommentsResponse
-) implements Serializable {
+) {
 
     public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
         return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
