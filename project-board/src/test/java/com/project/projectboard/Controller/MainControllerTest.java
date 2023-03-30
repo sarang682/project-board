652049f78db88e22bfe36093e67d74f726bd2bde
sarang682 <jms20914@gmail.com>
2023-03-30 16:15:36 +0900

    #56 - 실패한 테스트 코드 발견 및 수정
    정확한 원인은 알 수 없으나, 복구하고 테스트 완료.
    전체 테스트 통과함

--- project-board/src/test/java/com/project/projectboard/Controller/MainControllerTest.java
+++ project-board/src/test/java/com/project/projectboard/Controller/MainControllerTest.java
@@ -1,6 +1,7 @@
 package com.project.projectboard.controller;
 
-import com.project.projectboard.config.SecurityConfig;
+import com.project.projectboard.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,7 +12,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Import(SecurityConfig.class)
+@DisplayName("View 루트 컨트롤러")
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
@@ -21,6 +23,7 @@ class MainControllerTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("[view][GET] 루트 페이지 -> 게시글 리스트 (게시판) 페이지 Redirection")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         // Given
@@ -29,7 +32,6 @@ class MainControllerTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("forward:/articles"))
-                .andExpect(forwardedUrl("/articles"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(forwardedUrl("/articles"));
     }
 }
